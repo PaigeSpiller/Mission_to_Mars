f59a272918a759746375ca698b110a470ca43aa0
Paige Spiller <paige.spiller2@gmail.com>
2021-05-06 19:12:11 -0400

    updating bugs in code

--- scraping.py
+++ scraping.py
@@ -20,7 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "mars_image":mars_image(browser)
+        "hemispheres":mars_image(browser)
     }
 
     # Stop webdriver and return data
